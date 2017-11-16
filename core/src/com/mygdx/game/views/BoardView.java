976ae9725bdf6bf9f5fdba5f68de5d4604ee2c0c
Nathan Livsey <natlivsey@gmail.com>
2017-11-16 16:23:51 +0000

    Made changes to board view
    it now shows the hello world example

--- core/src/com/mygdx/game/views/BoardView.java
+++ core/src/com/mygdx/game/views/BoardView.java
@@ -1,10 +1,30 @@
 package com.mygdx.game.views;
 
-public class BoardView{
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-    public BoardView()
+public class BoardView extends ScreenAdapter{
+	private SpriteBatch batch;
+	private Texture logo;
+	
+	@Override
+    public void show()
     {
-        
+    	batch = new SpriteBatch();
+    	logo = new Texture(Gdx.files.internal("badlogic.jpg"));
     }
+	
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(0, 1, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		batch.begin();
+		batch.draw(logo,0,0);
+		batch.end();
+	}
 
 }
