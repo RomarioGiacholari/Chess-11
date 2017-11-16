fb6b4d8ef6143696651099122c4af3e733ac5fd8
Nathan Livsey <natlivsey@gmail.com>
2017-11-16 16:30:12 +0000

    stuff

--- core/src/com/mygdx/game/views/BoardView.java
+++ core/src/com/mygdx/game/views/BoardView.java
@@ -19,7 +19,7 @@ public class BoardView extends ScreenAdapter{
 	
 	@Override
 	public void render(float delta) {
-		Gdx.gl.glClearColor(0, 1, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
 		batch.begin();
