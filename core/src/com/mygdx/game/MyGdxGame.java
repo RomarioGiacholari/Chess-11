4ded3c366539412818c0ec9733b12abdc86d33ca
RomarioGiacholari <giacholari@gmail.com>
2017-12-07 14:31:09 +0000

    minor changes

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -13,31 +13,18 @@ import com.mygdx.game.controller.*;
 public class MyGdxGame extends ApplicationAdapter {
 	
 
-	private static final int GRID_CELL = 32;
 	 private ShapeRenderer shapeRenderer;
 	 
-	 public void show(){
+	 @Override
+	 public void create(){
 		 
 		 shapeRenderer = new ShapeRenderer();
 	 }
-
-	 private void drawGrid() {
-		 shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
-		 for (int x = 0; x < Gdx.graphics.getWidth(); x += GRID_CELL) {
-			 for (int y = 0; y < Gdx.graphics.getHeight(); y += GRID_CELL) {
-				 shapeRenderer.rect(x,y, GRID_CELL, GRID_CELL);
-			 }
-		 }
-		 shapeRenderer.end();
-	}
 	 
-	 public void render(float delta) {
-		// Other code omitted for now
-		drawGrid();
-		draw();
-		}
-
-	 shapeRenderer.rect(100,100, GRID_CELL, GRID_CELL);
+	 @Overrride
+	 public void render() {
+		
+	}
 
 }
 
