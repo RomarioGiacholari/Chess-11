6fe028a391f6e54f1dd38020b0fb28664263c587
RomarioGiacholari <giacholari@gmail.com>
2018-02-19 22:21:32 +0000

    added Bishop test stub

---
+++ core/src/com/mygdx/game/rules/BishopTest.java
@@ -0,0 +1,14 @@
+package com.mygdx.game.rules;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BishopTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
