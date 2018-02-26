b2c84e95660261d189c51b3cb9c7de6b2254fb14
RomarioGiacholari <giacholari@gmail.com>
2018-02-26 16:49:54 +0000

    added Bishop test

--- core/src/com/mygdx/game/rules/BishopTest.java
+++ core/src/com/mygdx/game/rules/BishopTest.java
@@ -7,8 +7,26 @@ import org.junit.Test;
 public class BishopTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_it_prints_the_piece_type() {
+		
+		Bishop bishop = new Bishop(1,4,true);
+		assertEquals("Bishop",bishop.printPieceType());	
+	}
+	
+	@Test
+	public void test_it_moves_the_piece_to_a_valid_location() {
+		
+		Bishop bishop = new Bishop(1,4,true);
+	
+		assertTrue(bishop.move(2,3));	
+	}
+	
+	@Test
+	public void test_it_cannot_move_the_piece_to_a_location_outside_the_board_limits() {
+		
+		Bishop bishop = new Bishop(1,4,true);
+	
+		assertFalse(bishop.move(9,9));	
 	}
 
 }
