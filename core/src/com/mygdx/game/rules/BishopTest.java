783f848668707c54ce25c8f53c2b9cf15e94b542
RomarioGiacholari <giacholari@gmail.com>
2018-03-19 18:12:17 +0000

    wip

--- core/src/com/mygdx/game/rules/BishopTest.java
+++ core/src/com/mygdx/game/rules/BishopTest.java
@@ -21,5 +21,13 @@ public class BishopTest {
 		
 		assertTrue(bishop.move(2,2));
 	}
+	
+	@Test
+	public void test_it_checks_for_invalid_moves() {
+		
+		Bishop bishop = new Bishop(1,3,true);
+		
+		assertFalse(bishop.move(5,5));
+	}
 
 }
