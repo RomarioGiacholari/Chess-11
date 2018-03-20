a9f7aef5b22cc7d5ae2ca2da425a4198b2b56037
Unknown <livseynj@aston.ac.uk>
2018-03-20 19:02:37 +0000

    setup for options

--- core/src/com/mygdx/game/models/Board.java
+++ core/src/com/mygdx/game/models/Board.java
@@ -47,6 +47,12 @@ public class Board {
 	 * A constructor for a Board object, it initialises the board array to be 8*8, the 
 	 * dimensions of a normal chess board.
 	 */
+	
+	private boolean pawns= true;
+	private boolean queens = true;
+	private boolean knights = true;
+	private boolean bishops = true;
+	private boolean rooks = true;
 	public Board() {
 		
 		board = new Piece[DIMENSIONS][DIMENSIONS];
@@ -59,39 +65,59 @@ public class Board {
 	 * its default positions on a chess board within the array.
 	 */
 	public void setUp() {
-		board [0][0] = new Rook(0,0,true);
-		board [0][1] = new Knight(0,1,true);
-		board [0][2] = new Bishop(0,2,true);
-		board [0][3] = new Queen(0,3,true);
-		board [0][4] = new King(0,4,true);
-		board [0][5] = new Bishop(0,5,true);
-		board [0][6] = new Knight(0,6,true);
-		board [0][7] = new Rook(0,7,true);
-		board [1][0] = new Pawn(1,0,true);
-		board [1][1] = new Pawn(1,1,true);
-		board [1][2] = new Pawn(1,2,true);
-		board [1][3] = new Pawn(1,3,true);
-		board [1][4] = new Pawn(1,4,true);
-		board [1][5] = new Pawn(1,5,true);
-		board [1][6] = new Pawn(1,6,true);
-		board [1][7] = new Pawn(1,7,true);
+		if(rooks) {
+			board [0][0] = new Rook(0,0,true);
+			board [0][7] = new Rook(0,7,true);
+			board [7][0] = new Rook(7,0,false);
+			board [7][7] = new Rook(7,7,false);
+
+		}
 		
-		board [7][0] = new Rook(7,0,false);
-		board [7][1] = new Knight(7,1,false);
-		board [7][2] = new Bishop(7,2,false);
-		board [7][3] = new Queen(7,3,false);
+		if (knights) {
+			board [0][1] = new Knight(0,1,true);
+			board [0][6] = new Knight(0,6,true);
+			board [7][1] = new Knight(7,1,false);
+			board [7][6] = new Knight(7,6,false);
+
+		}
+		
+		if(bishops) {
+			board [0][2] = new Bishop(0,2,true);
+			board [0][5] = new Bishop(0,5,true);
+			board [7][2] = new Bishop(7,2,false);
+			board [7][5] = new Bishop(7,5,false);
+
+		}
+		
+		if (queens) {
+			board [0][3] = new Queen(0,3,true);
+			board [7][3] = new Queen(7,3,false);
+		}
+		
+		if(pawns) {
+			board [1][0] = new Pawn(1,0,true);
+			board [1][1] = new Pawn(1,1,true);
+			board [1][2] = new Pawn(1,2,true);
+			board [1][3] = new Pawn(1,3,true);
+			board [1][4] = new Pawn(1,4,true);
+			board [1][5] = new Pawn(1,5,true);
+			board [1][6] = new Pawn(1,6,true);
+			board [1][7] = new Pawn(1,7,true);
+			
+			board [6][0] = new Pawn(6,0,false);
+			board [6][1] = new Pawn(6,1,false);
+			board [6][2] = new Pawn(6,2,false);
+			board [6][3] = new Pawn(6,3,false);
+			board [6][4] = new Pawn(6,4,false);
+			board [6][5] = new Pawn(6,5,false);
+			board [6][6] = new Pawn(6,6,false);
+			board [6][7] = new Pawn(6,7,false);
+		}
+		board [0][4] = new King(0,4,true);
+		
+		
 		board [7][4] = new King(7,4,false);
-		board [7][5] = new Bishop(7,5,false);
-		board [7][6] = new Knight(7,6,false);
-		board [7][7] = new Rook(7,7,false);
-		board [6][0] = new Pawn(6,0,false);
-		board [6][1] = new Pawn(6,1,false);
-		board [6][2] = new Pawn(6,2,false);
-		board [6][3] = new Pawn(6,3,false);
-		board [6][4] = new Pawn(6,4,false);
-		board [6][5] = new Pawn(6,5,false);
-		board [6][6] = new Pawn(6,6,false);
-		board [6][7] = new Pawn(6,7,false);
+		
 	}
 	
 	/**
@@ -563,6 +589,23 @@ public class Board {
 		}
 		
 		return false;
+	}
+	
+	public void switchPawn( boolean pawn) {
+		pawns = pawn;
+	}
+	
+	public void switchQueen(boolean queen) {
+		queens = queen;
+	}
+	public void switchKnight(boolean knight) {
+		knights = knight;
+	}
+	public void switchBishop(boolean bish) {
+		bishops = bish;
+	}
+	public void switchRook(boolean ro) {
+		rooks = ro;
 	}
 }	
 	
